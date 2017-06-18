cd3c761ff6f289a12e28fcbf70d961a46f63f581
Nishant Shelar <nishant@flywheel.com>
2017-06-18 11:17:32 +0530

    Added duplicate component check.

--- webapp/inventory/models.py
+++ webapp/inventory/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.postgres.fields import HStoreField
 
-# Create your models here.
+
 class Property(models.Model):
     name = models.CharField(max_length=100, unique=True)
     unit = models.CharField(max_length=100, null=True, blank=True)
@@ -16,12 +16,14 @@ class Property(models.Model):
     def __unicode__(self):
         return '%s' % self.name
 
+
 class ComponentType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     properties = models.ManyToManyField(Property, through='ComponentTypeProperties')
 
     def __unicode__(self):
         return '%s' % self.name
+
 
 class Component(models.Model):
     component_type = models.ForeignKey(ComponentType)
@@ -31,6 +33,17 @@ class Component(models.Model):
 
     def __unicode__(self):
         return '%s_%s' % (self.component_type.name, self.id)
+
+    def validate_unique(self, exclude=None):
+        super(Component, self).validate_unique(exclude)
+        existing_count = self.__class__.objects.filter(component_type=self.component_type, component_data__contains=self.component_data).count()
+        if existing_count != 0:
+            raise ValidationError('Component already exists.')
+
+    def save(self, *args, **kwargs):
+        self.validate_unique()
+        super(Component, self).save(*args, **kwargs)
+
 
 class ComponentTypeProperties(models.Model):
     component_type = models.ForeignKey(ComponentType, on_delete=models.CASCADE)
